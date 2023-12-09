dad35cade814e17793b2c1a4326e909d6850357d
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-12-09 22:27:58 +0530

    created billboards and featured product

---
+++ components/ui/currency.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const formatter = new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  });
+
+interface CurrencyProps {
+  value?: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({
+  value = 0
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return ( 
+    <div className="font-semibold">
+      {formatter.format(Number(value))}
+    </div>
+  );
+}
+
+export default Currency;
