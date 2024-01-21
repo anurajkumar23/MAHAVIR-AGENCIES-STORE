735846d8f1bd9deb2b68be62a62fdb1de858f93f
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-01-21 21:51:01 +0530

    changes

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("d0a74e76-5431-403a-a5e8-58cacd8f7374");
 
   return (
     <Container>
